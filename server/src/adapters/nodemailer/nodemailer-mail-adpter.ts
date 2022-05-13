62aafc86600fe6dac6465826484153561f7ce0f3
Luiz Henrique Cabral <42081493+xZANELLA@users.noreply.github.com>
2022-05-13 11:33:04 -0300

    feat: add nodemailer-mail-adapter

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adpter.ts
@@ -0,0 +1,24 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer'
+import { brotliDecompressSync } from "zlib";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "7c0bf8c9a8b4d2",
+        pass: "874e36cd5c1bd6"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body}: SendMailData) {
+
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Luiz Henrique Zanella <lhenriquezan@gmail.com>',
+            subject,
+            html: body,
+        })
+    }
+}
