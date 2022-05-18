3e975d15703a7b1ce2ddd88bcc365efbb1b95aa8
Luiz Henrique Cabral <42081493+xZANELLA@users.noreply.github.com>
2022-05-18 17:32:54 -0300

    feat: update component Widget

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
@@ -6,8 +6,19 @@ import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { Options } from '../Options';
+import { Form } from '../Form';
+import { Success } from '../Success';
+
+
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export type FeedbackType = keyof typeof feedbackTypes
 
 function Widget() {
+
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false)
 
     const bottomSheetRef = useRef<BottomSheet>(null)
 
@@ -15,29 +26,56 @@ function Widget() {
         bottomSheetRef.current?.expand()
     }
 
-  return (
-    <>
-        <TouchableOpacity
-            style={styles.button}
-            onPress={handleOpen}
-        >
-            <ChatTeardropDots 
-                size={24}
-                weight="bold"
-                color={theme.colors.text_on_brand_color}
-            />
-        </TouchableOpacity>
+    function handleRestartFeedback() {
+        setFeedbackType(null)
+        setFeedbackSent(false)
+    }
 
-        <BottomSheet
-            ref={bottomSheetRef}
-            snapPoints={[1, 280]}
-            backgroundStyle={styles.modal}
-            handleIndicatorStyle={styles.indicator}
-        >
+    function handleFeedbackSent() {
+        setFeedbackSent(true)
+    }
 
-        </BottomSheet>
-    </>
-  );
+    return (
+        <>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={handleOpen}
+            >
+                <ChatTeardropDots
+                    size={24}
+                    weight="bold"
+                    color={theme.colors.text_on_brand_color}
+                />
+            </TouchableOpacity>
+
+            <BottomSheet
+                ref={bottomSheetRef}
+                snapPoints={[1, 280]}
+                backgroundStyle={styles.modal}
+                handleIndicatorStyle={styles.indicator}
+            >
+                {
+                    feedbackSent ?
+                        <Success onSendAnotherFeedback={handleRestartFeedback} />
+                        :
+                        <>
+                            {
+                                feedbackType ?
+                                    <Form
+                                        feedbackType={feedbackType}
+                                        onFeedbackCanceled={handleRestartFeedback}
+                                        onFeedbackSent={handleFeedbackSent}
+                                    />
+                                    :
+                                    <Options onFeedbackTypeChanged={setFeedbackType} />
+                            }
+
+                        </>
+                }
+
+            </BottomSheet>
+        </>
+    );
 }
 
 export default gestureHandlerRootHOC(Widget)
